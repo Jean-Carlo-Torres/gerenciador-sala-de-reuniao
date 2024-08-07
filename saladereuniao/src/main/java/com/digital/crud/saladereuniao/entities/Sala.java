414889d7c446ee4268e14d4f79f3b2f2d33a81cf
Jean Carlo <jeansanto2002@gmail.com>
2024-08-07 17:23:05 -0300

    Base inicial do projeto

---
+++ saladereuniao/src/main/java/com/digital/crud/saladereuniao/entities/Sala.java
@@ -0,0 +1,62 @@
+package com.digital.crud.saladereuniao.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "saladereuniao")
+public class Sala {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String data;
+
+    @Column(nullable = false)
+    private String hora;
+
+    public Sala() {
+    }
+
+    public Sala(String nome, String data, String hora) {
+        this.nome = nome;
+        this.data = data;
+        this.hora = hora;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+}
